31f2f7df9a972a56470fb9f3b16a46251fe56299
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-08 20:26:28 +0300

    Update MyArrayListApp.java

--- MyArrayListApp.java
+++ MyArrayListApp.java
@@ -1,1 +1,21 @@
+package com.company;
 
+public class MyArrayListApp {
+
+    public static void main(String[] args) {
+        MyArrayList<String> myArrayList = new MyArrayList<>();
+        MyArrayList<String> copyArrayList = new MyArrayList<>();
+        myArrayList.isEmpty();
+        myArrayList.add("1");
+        myArrayList.add("2");
+        myArrayList.add("3");
+        myArrayList.add("4", 6);
+        myArrayList.get(3);
+        myArrayList.remove(2);
+        myArrayList.remove("1");
+        myArrayList.set("6", 7);
+        copyArrayList.addAll(myArrayList);
+        copyArrayList.printArrayList();
+        myArrayList.printArrayList();
+    }
+}
